d7877544f36cd43c00793e073fa6abe0709babc5
ELIAN <jevillaurrutia@gmail.com>
2022-12-15 14:51:12 -0500

    Subida de archivos

--- components/message/store.js
+++ components/message/store.js
@@ -1,57 +1,49 @@
-const db = require("mongoose");
-const Model = require("./model");
-
-
-
-
-const list = [];
+const Model = require('./model');
 
 function addMessage(message) {
-  const myMessage = new Model(message);
-  myMessage.save();
+    const myMessage = new Model(message);
+    myMessage.save();
 }
 
-async function getMessages(filterUser) {
-  /* return list; */
-  return new Promise((resolve, reject) => {
-  let filter = {};
-  
-  if (filterUser !== null) {
-    filter = { user: filterUser };
-    console.log(filter);
-  }
-  Model.find(filter)
-  .populate('user') 
-  .exec((error, populated) => {
-    if (error) {
-      reject(error);
-      return false;
-    }
-    resolve(populated);
-  });
-  });
-}
+async function getMessages(filterChat) {
+    return new Promise((resolve, reject) => {
+        let filter = {};
+        if (filterChat !== null) {
+            filter = { chat: filterChat };
+        }
+        Model.find(filter)
+            .populate('user')
+            .exec((error, populated) => {
+                if (error) {
+                    reject(error);
+                    return false;
+                }
 
-async function updateText(id, message) {
-  const foundMessage = await Model.findOne({
-    _id: id,
-  });
-
-  foundMessage.message = message;
-
-  const newMessage = await foundMessage.save();
-  return newMessage;
+                resolve(populated);
+            });
+    })
 }
 
 function removeMessage(id) {
-  return Model.deleteOne({
-    _id: id,
-    
-  })
+    return Model.deleteOne({
+        _id: id
+    });
 }
+
+async function updateText(id, message) {
+    const foundMessage = await Model.findOne({
+        _id: id
+    });
+
+    foundMessage.message = message;
+
+    const newMessage = await foundMessage.save();
+    return newMessage;
+}
+
 module.exports = {
-  add: addMessage,
-  list: getMessages,
-  updateText: updateText,
-  remove: removeMessage,
-};
+    add: addMessage,
+    list: getMessages,
+    updateText: updateText,
+    remove: removeMessage,
+}
--- network/routes.js
+++ network/routes.js
@@ -4,6 +4,7 @@ const express = require("express");
 const routes = function (server) {
     server.use("/message", require("../components/message/network"));
     server.use("/user", require("../components/message/user/network"));
+    server.use("/chat", require("../components/message/chat/network"));
 };
 
 module.exports = routes;
